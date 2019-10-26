41294173a3f5aae0efca998c39448ca1c67f556c
mike-petrov <mis5116@yandex.ru>
2019-10-26 16:57:52 +0300

    new design Task

--- web/src/Components/TasksList/Task/Task.jsx
+++ web/src/Components/TasksList/Task/Task.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Task.css';
+import { dateFormat } from '../../../Functions/handle';
 
 
 const Task = (props) => {
@@ -9,12 +10,24 @@ const Task = (props) => {
 		task,
 	} = props;
 	return (
-		<div className="task">
-			{task.id}
-			{task.image}
-			{task.text}
-			{task.tags}
-		</div>
+		<Link to={`card/${task.id}`} className="task">
+			<div className="task_left">
+				<img src={task.image} alt="" />
+			</div>
+			<div className="task_right">
+				<div className="task_title">{task.text}</div>
+				<div className="task_date">{dateFormat(task.time * 1000)}</div>
+				<div className="task_tags">
+					{task.tags.map((tag) => (
+						<span className="tag">{tag}</span>
+					))}
+				</div>
+				<div className="task_user">
+					<i className="fas fa-user" />
+					{task.user}
+				</div>
+			</div>
+		</Link>
 	);
 };
 
