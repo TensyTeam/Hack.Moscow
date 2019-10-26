ba56cef80f8630fa20fcf5d33a7cf3cc066766b5
mike-petrov <mis5116@yandex.ru>
2019-10-26 13:38:34 +0300

    update route containers

---
+++ web/src/App.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+	BrowserRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
+
+import Home from './Containers/Home.jsx';
+import Profile from './Containers/Profile.jsx';
+import Popup from './Containers/Popup.jsx';
+
+import Header from './Components/Header/Header.jsx';
+import Footer from './Components/Footer/Footer.jsx';
+
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showPopup: { active: false, current: null },
+			redirect: { status: false, path: '/' },
+		};
+		this.onPopup = this.onPopup.bind(this);
+		this.onRedirect = this.onRedirect.bind(this);
+	}
+
+	onPopup(_active, _current) {
+		this.setState({ showPopup: { active: _active, current: _current } });
+	}
+
+	onRedirect(_path) {
+		this.setState({ redirect: { status: true, path: _path } });
+	}
+
+	render() {
+		const {
+			showPopup, redirect,
+		} = this.state;
+		return (
+			<BrowserRouter>
+				<div className="module">
+					{showPopup.active && (
+						<Popup
+							showPopup={showPopup}
+							onPopup={this.onPopup}
+							onRedirect={this.onRedirect}
+						/>
+					)}
+					<Header />
+					<Switch>
+						{redirect.status === true && (
+							<>
+								<Redirect to={redirect.path} />
+								{this.setState({ redirect: { status: false, path: redirect.path } })}
+							</>
+						)}
+						<Route exact path="/">
+							<Home
+								onPopup={this.onPopup}
+								onRedirect={this.onRedirect}
+							/>
+						</Route>
+						<Route exact path="/profile">
+							<Profile
+								onPopup={this.onPopup}
+								onRedirect={this.onRedirect}
+							/>
+						</Route>
+					</Switch>
+				</div>
+				<Footer />
+			</BrowserRouter>
+		);
+	}
+}
